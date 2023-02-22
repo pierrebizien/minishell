6fab29720c10ae5a22437662a74186744036c753
Pierre BIZIEN <pbizien@e1r13p10.clusters.42paris.fr>
2023-02-22 16:38:50 +0100

    recup env

--- minishell_src/ft_parsing.c
+++ minishell_src/ft_parsing.c
@@ -4,33 +4,37 @@ extern t_data data;
 
 int ft_1st_chev(void)
 {
-    if (data.ac <= 2)
-        return (write(2, "syntax error\n", 13), 2);
+	 if (data.ac <= 2)
+		  return (write(2, "syntax error\n", 13), 2);
 	// if (data.ac == 3)
-    return (0);
+	 return (0);
 }
 
 int ft_detect(void)
 {
-    if (data.args && ft_strncmp(data.args[0], "<", ft_strlen(data.args[0])) == 0)
-    {
-        return (ft_1st_chev());
-    }
-    return (0);
+	 if (data.args && ft_strncmp(data.args[0], "<", ft_strlen(data.args[0])) == 0)
+	 {
+		  return (ft_1st_chev());
+	 }
+	 return (0);
 }
 
 int ft_parse(char *str)
 {
-    data.args = ft_split(str, ' ');
-    if (!data.args)
-        return (1);
-    
-    return(ft_detect());
-    return (0);
+	 data.args = ft_detect_cmd();
+	 if (!data.args)
+		  return (1);
+	 
+	 return(ft_detect());
+	 return (0);
 }
 
-int ft_prep_pipex(void)
+int ft_detect_cmd(void)
 {
-    if (ft_strncmp(data.args[0], "<", ft_strlen(data.args[0])) == 0)
-        {}
+	int  count_w;
+
+	count_w = 0;
+	ft_split_mini();
+
+	return (0);
 }
