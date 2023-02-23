58ff5cfc567d790c4dbed5556a3ab5f61c9dba63
Pierre BIZIEN <pbizien@e2r1p13.clusters.42paris.fr>
2023-02-23 16:45:49 +0100

    regler le retour du prompt apres mauvaises quotes

--- minishell_src/ft_init.c
+++ minishell_src/ft_init.c
@@ -6,6 +6,8 @@ int ft_init(char **argv, char **envp, int ac)
 {
 	data.ac = ac;
 	data.av = argv;
+	data.pip.in_fd = 0; 
+	data.pip.out_fd = 1; 
 	data.sep = "|";
 	ft_create_env(envp);
 	return (0);
