dcc68b8c59533d984a6492cd80ca001264f94297
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-02-24 12:40:11 +0100

    clc_minishell

--- libft/src/ft_strlen.c
+++ libft/src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:27:41 by ngriveau          #+#    #+#             */
-/*   Updated: 2023/02/22 15:59:15 by ngriveau         ###   ########.fr       */
+/*   Updated: 2023/02/24 10:51:23 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,14 @@ size_t	ft_strlen(const char *str)
 	{
 		i++;
 	}
+	return (i);
+}
+size_t	ft_strlen_WS(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (!(str[i] == '\0'))
+		i++;
 	return (i);
 }
--- minishell_src/ft_cmd_export.c
+++ minishell_src/ft_cmd_export.c
@@ -2,6 +2,34 @@
 
 t_data data;
 
+int	is_ws(char c)
+{
+	int	i;
+	const char ws[] = " \n\t\f\r\v";
+
+	i = 0;
+	while (ws[i])
+	{
+		if (c == ws[i])
+			return (1);
+		i++;
+	}
+	return (0);
+	
+}
+
+size_t	ft_strlen_WS(const char *str)
+{
+	size_t	i;
+	int		j;
+	char	*ws;
+
+	ws = WS;
+	i = 0;
+	while (str[i] && is_ws(str[i]) == 0)
+		i++;
+	return (i);
+}
 
 static int ft_just_export(void)
 {
@@ -27,17 +55,20 @@ static int ft_plus_egal_export(char *str)
     t_env	*tmp;
 	char	*var;
 	int i;
+	int k;
 
 	i = 0;
+	k = 0;
 	while (str[i] != '=' && str[i] != '+' && str[i])
 		i++;
 	if (str[i] == '=')
 		return (1);
 	tmp = &data.env;
 	var = ft_substr(str, 0, i);
-	// fprintf(stderr, "var = |%s|%s|%d\n\n", var,str + i + 2,  i);
 	while (tmp->next && ft_strncmp(var, tmp->key, i))
 		tmp = tmp->next;
+	while (str[k + i + 1] && is_ws(str[k + i + 1]) == 0)
+		k++;
 	i = i + 2;
 	if (ft_strncmp(var, tmp->key, i) == 0)
 		tmp->value = ft_strjoin(tmp->value, str + i);
@@ -55,12 +86,18 @@ static int ft_verif_str_export(char *str)
 {
 	int i;
 
-	i = -1;
+	i = 0;
+	if (ft_isdigit(str[i]) == 1)
+		return (1);
 	while (str && str[++i] != '=' && str[i] != '+')
 	{
+		if (is_ws(str[i]))
 		if(ft_isalnum(str[i]) == 0 && str[i] != '_')
 			return (1);
 	}
+	if (str[i] == '+' && str[i + 1] != '=')
+		return (1);
+	
 	return (0);
 }
 
@@ -74,7 +111,7 @@ static int ft_ok_export(char *str, t_env *tmp)
 	while (str[j] != '=' && str[j])
 		j++;
 	k = 0;
-	while (str[k + j + 1])
+	while (str[k + j + 1] && is_ws(str[k + j + 1]) == 0)
 		k++;
 	tmp->key = ft_substr(str, 0, j);
 	if (!tmp->key)
@@ -93,13 +130,21 @@ int ft_export(char *str)
 
 	i = 0;
 	tmp = &data.env;
+	ft_strlen_WS(str);
 	if (ft_strncmp(str, "export", 7) == 0)
 		return (ft_just_export());
 	while (str[i] && str[i] != ' ')
 		i++;
 	i++;
-	if (ft_verif_str_export(str + i) == 1)
-		return (ft_printf("export: `%s': not a valid identifier", str), 1);
+	while (str[i])
+	{
+		if (ft_verif_str_export(str + i))
+			ft_printf("export: `%s': not a valid identifier\n", ft_substr(str, i, ft_strlen_WS(str + i)));
+		i = i + ft_strlen_WS(str + i);
+		while (str[i] && is_ws(str[i]) == 1)
+			i++;
+
+	}
 	if (ft_plus_egal_export(str + i) == 0)
 		return (0);
 	while (tmp->next)
@@ -107,6 +152,7 @@ int ft_export(char *str)
 	tmp->next = ft_lstnew_env();
 	tmp = tmp->next;
 	ft_ok_export(str + i, tmp);
+
 	return (0);
 }
 
@@ -116,23 +162,35 @@ int main(int c, char **v, char **e)
 	ft_create_env(e);
 
     char *test;
-	// test = "export";
-    // fprintf(stderr, "\t\t1|%d|\n", ft_export(test));
+	test = "export";
+    fprintf(stderr, "\t\t\e[35m1|%d|\e[0m\n", ft_export(test));
 
 	test = "export test=42";
-    fprintf(stderr, "\t\t2|%d|\n", ft_export(test));
+    fprintf(stderr, "\t\t\e[35m2|%d|\e[0m\n", ft_export(test));
 
 	test = "export test+=pierretest";
-    fprintf(stderr, "\t\t3|%d|\n", ft_export(test));
+    fprintf(stderr, "\t\t\e[35m3|%d|\e[0m\n", ft_export(test));
 
 	test = "export test_2+=pierretest";
-    fprintf(stderr, "\t\t4|%d|\n", ft_export(test));
+    fprintf(stderr, "\t\t\e[35m4|%d|\e[0m\n", ft_export(test));
+
+	test = "export testspace=4  2 sansegal";
+    fprintf(stderr, "\t\t\e[35m5|%d|\e[0m\n", ft_export(test));
+
+	test = "export testspace=4  2 test=test";
+    fprintf(stderr, "\t\t\e[35m5.2|%d|\e[0m\n", ft_export(test));
 
 	test = "export test@=oui";
-    fprintf(stderr, "\t\t5|%d|\n", ft_export(test));
+    fprintf(stderr, "\t\t\e[35m6.1|%d|\e[0m\n", ft_export(test));
 
-		test = "export test=oui";
-    fprintf(stderr, "\t\t6|%d|\n", ft_export(test));
+	test = "export 1test=oui";
+    fprintf(stderr, "\t\t\e[35m6.2|%d|\e[0m\n", ft_export(test));
+
+	test = "export test =oui";
+    fprintf(stderr, "\t\t\e[35m6.3|%d|\e[0m\n", ft_export(test));
+
+	test = "export test+ =oui";
+    fprintf(stderr, "\t\t\e[35m6.4|%d|\e[0m\n", ft_export(test));
 
 
 
