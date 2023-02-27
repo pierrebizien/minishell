3f228588f6671175e31a93a46cd15822ed5d0e16
Pierre BIZIEN <pbizien@e1r7p19.clusters.42paris.fr>
2023-02-27 13:07:34 +0100

    merge 2702

--- minishell_src/ft_cmd_echo.c
+++ minishell_src/ft_cmd_echo.c
@@ -2,16 +2,3 @@
 
 extern t_data data;
 
-int ft_echo(int start, int end)
-{
-    int newline;
-    if (data.args[start] == 0)
-        return (0);
-}
-
-int main(void)
-{
-    char *cmd;
-    cmd = "echo -n test numero 1";
-    ft_echo(1, 4);
-}
