81c06ccc35d5f88163ab655bde459dbc4a5c3b25
Pierre BIZIEN <pbizien@e1r11p19.clusters.42paris.fr>
2023-03-06 16:40:56 +0100

    implementation buitin et multi pipe

--- minishell_src/ft_init.c
+++ minishell_src/ft_init.c
@@ -10,6 +10,7 @@ int ft_init(char **env, t_data *data)
 	data->pip.fd_in = 0;
 	data->pip.fd_out = 1;
 	data->pip.hd_in = 0;
+	ft_init_pipex_pipe(data);
 	ft_create_env(env, data);
 	return (0);
 }
--- minishell_src/ft_pipex.c
+++ minishell_src/ft_pipex.c
@@ -1,5 +1,6 @@
 #include "./minishell.h"
 
+int id2;
 
 char **ft_join_dstr(char **dest, char* src)
 {
@@ -36,7 +37,7 @@ char **ft_get_paths(t_data *data)
     {
         if (ft_strncmp(tmp->key, "PATH", ft_strlen(tmp->key)) == 0)
         {
-            fprintf(stderr, "tmp->key = |%s|\t value = |%s|\n\n", tmp->key, tmp->value);
+            // fprintf(stderr, "tmp->key = |%s|\t value = |%s|\n\n", tmp->key, tmp->value);
             return (ft_split(tmp->value, ':'));
         }
         tmp = tmp->next;
@@ -51,10 +52,13 @@ char	*find_path(char **cmd, char **paths_env)
 
 	i = 0;
 	// fprintf(stderr, "ICI 2 cmd[0] %s\n", cmd[0]);
-	if (!access(cmd[0], F_OK))
+	if (!access(cmd[0], R_OK) && (cmd[0][0] != '.' || cmd[0][0] != '/'))
 	{
 		if (access(cmd[0], X_OK))
 			return (perror(cmd[0]), exit(errno), NULL);
+		else if (cmd[0][0] != '.' && cmd[0][0] != '/')
+			return (ft_putstr_fd(cmd[0], 2), ft_putstr_fd(": Command not found\n", 2),\
+				exit(errno), NULL);
 		else
 			return (ft_strdup(cmd[0]));
 	}
@@ -73,7 +77,7 @@ char	*find_path(char **cmd, char **paths_env)
 		i++;
 		free(tmp);
 	}
-	return (ft_putstr_fd(cmd[0], 2), ft_putstr_fd(": Command not found", 2),\
+	return (ft_putstr_fd(cmd[0], 2), ft_putstr_fd(": Command not found\n", 2),\
 		 exit(errno), NULL);
 }
 
@@ -84,7 +88,7 @@ int	contain_token(t_exec* begin, int token, int m)
 
 	tmp = begin;
 	count = 0;
-	while (begin && count <= m)
+	while (begin && count < m)
 	{
 		if (begin->id == F_PIPE)
 			count++;
@@ -100,30 +104,62 @@ int	contain_token(t_exec* begin, int token, int m)
 	return (0);
 }
 
+void ft_print_fd(t_data *data)
+{
+	fprintf(stderr, "fd in %d\n", data->pip.fd_in);
+	fprintf(stderr, "fd out %d\n", data->pip.fd_out);
+	fprintf(stderr, "pipefd1 [0] %d\n", data->pip.pipefd1[0]);
+	fprintf(stderr, "pipefd1 [1] %d\n", data->pip.pipefd1[1]);
+	fprintf(stderr, "pipefd2 [0]  %d\n", data->pip.pipefd2[0]);
+	fprintf(stderr, "pipefd2 [1] %d\n", data->pip.pipefd2[1]);
+}
+
 void ft_dup_manage(t_data *data, int m)
 {
-	if (contain_token(&data->exec, F_INFILE, m) || contain_token(&data->exec, F_DELIMITER, m))
+	// fprintf(stderr, "ON DUP m vaut %d\n", m);
+	if (contain_token(&data->exec, F_INFILE, m) || contain_token(&data->exec, F_DELIMITER, m) || !m)
+	{
+		// fprintf(stderr, "DUP V0\n");
 		dup2(data->pip.fd_in, 0);
-	else if (m != 0 && m % 2 == 0)
-	{
-		ft_close(&data->pip.pipefd1[1]);
-		dup2(data->pip.pipefd1[0], 0);
-		ft_close(&data->pip.pipefd1[0]);
-		ft_close(&data->pip.pipefd2[0]);
-		dup2(data->pip.pipefd2[1], 1);
-		ft_close(&data->pip.pipefd2[1]);
 	}
-	if (contain_token(&data->exec, F_APPEND, m) || contain_token(&data->exec, F_TRONC, m))
-		dup2(data->pip.fd_out, 1);
-	else if (m != data->pip.nb_pipes && m % 2 == 1)
+	else if (m % 2 == 0)
 	{
+		// fprintf(stderr, "DUP V1\n");
 		ft_close(&data->pip.pipefd2[1]);
 		dup2(data->pip.pipefd2[0], 0);
 		ft_close(&data->pip.pipefd2[0]);
+		// ft_close(&data->pip.pipefd2[0]);
+		// dup2(data->pip.pipefd2[1], 1);
+		// ft_close(&data->pip.pipefd2[1]);
+	}
+	else
+	{
+		// fprintf(stderr, "DUP V2\n");
+		ft_close(&data->pip.pipefd1[1]);
+		dup2(data->pip.pipefd1[0], 0);
+		ft_close(&data->pip.pipefd1[0]);
+	}
+	if (contain_token(&data->exec, F_APPEND, m) || contain_token(&data->exec, F_TRONC, m) || m == data->pip.nb_pipes)
+	{
+		// fprintf(stderr, "DUP V3\n");
+		dup2(data->pip.fd_out, 1);
+	}
+	else if (m % 2 == 0)
+	{
+		// fprintf(stderr, "DUP V4 data pipefd1[1] = %d\n", data->pip.pipefd1[1]);
 		ft_close(&data->pip.pipefd1[0]);
 		dup2(data->pip.pipefd1[1], 1);
 		ft_close(&data->pip.pipefd1[1]);
 	}
+	else if (m % 2 == 1)
+	{
+		// fprintf(stderr, "DUP V5\n");
+		ft_close(&data->pip.pipefd2[0]);
+		dup2(data->pip.pipefd2[1], 1);
+		ft_close(&data->pip.pipefd2[1]);
+	}
+	// fprintf(stderr, "APRES DUP FD VALENT\n");
+	// ft_print_fd(data);
 }
 
 void	ft_exec_cmd(t_data *data, char **cmd, int m)
@@ -135,13 +171,26 @@ void	ft_exec_cmd(t_data *data, char **cmd, int m)
 	paths_env = ft_get_paths(data);
 	// perror("EXEC");
 	path_exec = find_path(cmd, paths_env);
+	(void)m;
+	// fprintf(stderr, "ID VAUT %d \n\n\n", id2);
 	ft_dup_manage(data, m);
 	// dup2(data->pip.fd_in, 0);
+	if (ft_test_builtin(cmd[0], data) == 1)
+	{
+		ft_close_all(data->pip);
+		exit(data->err_built_in);
+	}
 	// dup2(data->pip.fd_out, 1);
-	fprintf(stderr, "fd out vaut %d\n", data->pip.fd_out);
+	// fprintf(stderr, "avant exec out %d\n", data->pip.fd_out);
 	// fprintf(stderr, "PATH EXEC VAUT %s\n", path_exec);
 	execve(path_exec, cmd, paths_env);
-	fprintf(stderr, "PROBLEME D EXEC\n");
+	if (errno == 13)
+	{
+		ft_putstr_fd(cmd[0],2);
+		ft_putstr_fd(": Is a directory\n", 2);
+		exit(errno);
+	}
+	// fprintf(stderr, "PROBLEME D EXEC\n");
 	perror("EXEC");
 	exit(errno);
 	// ft_print_dchar(paths_env);
@@ -164,15 +213,15 @@ void	ft_child_exec(t_exec *begin, t_data *data, int m)
 
 	cmd = NULL;
 	// fprintf(stderr, "test begin->str %s\n", begin->next->str);
-	fprintf(stderr, "HELLLO\n");
 	while (begin && begin->id != F_PIPE)
 	{
-	fprintf(stderr, "test begin->str %s\n", begin->str);
+	// fprintf(stderr, "test begin->str %s\n", begin->str);
 		if (begin->id == F_FALSEI)
 		{
 			tmp_fd = open(begin->str, O_RDWR);
 			if (tmp_fd == -1)
 			{
+				// fprintf(stderr, "HELLLO1\n");
 				perror(begin->str);
 				exit(errno);
 			}
@@ -183,6 +232,7 @@ void	ft_child_exec(t_exec *begin, t_data *data, int m)
 			tmp_fd = open(begin->str, O_RDWR | O_APPEND | O_CREAT, 0644);
 			if (tmp_fd == -1)
 			{
+				// fprintf(stderr, "HELLLO2\n");
 				perror(begin->str);
 				exit(errno);
 			}
@@ -193,6 +243,7 @@ void	ft_child_exec(t_exec *begin, t_data *data, int m)
 			tmp_fd = open(begin->str, O_CREAT | O_RDWR | O_TRUNC, 0644);
 			if (tmp_fd == -1)
 			{
+				// fprintf(stderr, "HELLLO3\n");
 				perror(begin->str);
 				exit(errno);
 			}
@@ -211,7 +262,7 @@ void	ft_child_exec(t_exec *begin, t_data *data, int m)
 			if (!cmd)
 				return ; //GERER
 
-			ft_print_dchar(cmd);
+			// ft_print_dchar(cmd);
 
 		}
 		else if (begin->id == F_APPEND)
@@ -219,6 +270,7 @@ void	ft_child_exec(t_exec *begin, t_data *data, int m)
 			data->pip.fd_out = open(begin->str, O_CREAT | O_RDWR | O_APPEND, 0644);
 			if (data->pip.fd_out == -1)
 			{
+				// fprintf(stderr, "HELLLO4\n");
 				perror(begin->str);
 				exit(errno);
 			}
@@ -228,15 +280,17 @@ void	ft_child_exec(t_exec *begin, t_data *data, int m)
 			data->pip.fd_out = open(begin->str, O_CREAT | O_RDWR | O_TRUNC, 0644);
 			if (data->pip.fd_out == -1)
 			{
+				// fprintf(stderr, "HELLLO5\n");
 				perror(begin->str);
 				exit(errno);
 			}
 		}
 			else if (begin->id == F_INFILE)
 		{
-			data->pip.fd_in = open(begin->str, O_RDONLY | O_TRUNC, 0644);
+			data->pip.fd_in = open(begin->str, O_RDONLY, 0644);
 			if (data->pip.fd_in == -1)
 			{
+				// fprintf(stderr, "HELLLO6\n");
 				perror(begin->str);
 				exit(errno);
 			}
@@ -259,23 +313,32 @@ void	ft_pipex(t_data *data)
 	m = 0;
 	while (begin)
 	{
-		if (m % 2 == 0)
+		if (!m || m % 2 == 0)
 			pipe(data->pip.pipefd1);
 		else
 			pipe(data->pip.pipefd2);
 		id = fork();
 		if (id == 0)
 			ft_child_exec(begin, data, m);
-		while (wait(NULL) != -1)
-			(void)id;
-		fprintf(stderr, "id vaut %d\n", id);
+		if (!m || m % 2 == 0)
+		{
+			ft_close(&data->pip.pipefd2[0]);
+			ft_close(&data->pip.pipefd1[1]);
+		}
+		else
+		{
+			ft_close(&data->pip.pipefd1[0]);
+			ft_close(&data->pip.pipefd2[1]);
+		}
 		while (begin && begin->id != F_PIPE)
 			begin = begin->next;
-		// ft_reset_param_pip(data);
 		if (begin && begin->id == F_PIPE)
 			begin = begin->next;
 		m++;
 	}
+		while (wait(NULL) != -1)
+			(void)begin;
+		ft_close_all(data->pip);
 }
 
 
