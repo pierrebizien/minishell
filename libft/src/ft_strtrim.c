aa675f10500b4f084f16bb19775a2ef65e9319f1
Pierre BIZIEN <pbizien@e1r11p19.clusters.42paris.fr>
2023-03-06 18:18:44 +0100

    maj trim

--- libft/src/ft_strtrim.c
+++ libft/src/ft_strtrim.c
@@ -1,36 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbizien <pbizien@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 14:40:05 by ngriveau          #+#    #+#             */
+/*   Updated: 2023/03/06 18:17:10 by pbizien          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "./inc/SuperLibft.h"
-#include <stdio.h>
+#include "SuperLibft.h"
 
-static int	ft_is_in_set(char c, char const *set)
+static char	*find_begin(char const *s1, char const *set)
 {
-	int	i;
+	size_t	i;
+	size_t	j;
+	int		in_set;
 
+	in_set = 0;
 	i = 0;
-	while (set[i])
+	j = 0;
+	while (s1[i])
 	{
-		if (set[i] == c)
-			return (1);
+		in_set = 0;
+		j = 0;
+		while (set[j])
+		{
+			if (s1[i] == set[j])
+				in_set = 1;
+			j++;
+		}
+		if (!in_set)
+			break ;
 		i++;
 	}
-	return (0);
+	return ((char *) s1 + i);
+}
+
+static char	*find_end(char const *s1, char const *set, char const *begin)
+{
+	size_t	i;
+	size_t	j;
+	int		in_set;
+
+	in_set = 0;
+	i = ft_strlen(s1) - 1;
+	j = 0;
+	while (s1 + i >= begin)
+	{
+		in_set = 0;
+		j = 0;
+		while (set[j])
+		{
+			if (s1[i] == set[j])
+				in_set = 1;
+			j++;
+		}
+		if (!in_set)
+			break ;
+		i--;
+	}
+	if (s1 + i < begin)
+		return ((char *) begin);
+	return ((char *) s1 + i);
+}
+
+static char	*fill_str(char const *begin, char const *end)
+{
+	char	*new;
+	size_t	i;
+
+	new = malloc(sizeof(char) * (end - begin + 2));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (begin + i <= end)
+	{
+		new[i] = begin[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char			*str;
-	int				i;
-	unsigned int	start;
+	char	*begin;
+	char	*end;
+	char	*new;
 
-	i = 0;
-	while (s1[i] && ft_is_in_set(s1[i], set))
-		i++;
-	start = i;
-	while (s1[i] && !ft_is_in_set(s1[i], set))
-		i++;
-	str = ft_calloc(i - start + 1, sizeof(char));
-	if (!str)
+	begin = find_begin(s1, set);
+	end = find_end(s1, set, s1);
+	if (!s1[0] || end < begin)
+	{
+		new = malloc(sizeof(char) * 1);
+		if (!new)
+			return (NULL);
+		new[0] = '\0';
+	}
+	else
+		new = fill_str(begin, end);
+	if (!new)
 		return (NULL);
-	ft_memcpy(str, s1 + start, i - start);
-	return (str);
+	return (new);
 }
