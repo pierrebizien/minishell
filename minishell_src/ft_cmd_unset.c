941b1d9e68c44ca4d56154ad6e259a7bf9c268da
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-03-08 19:56:52 +0100

    unset k

--- minishell_src/ft_cmd_unset.c
+++ minishell_src/ft_cmd_unset.c
@@ -18,27 +18,21 @@ int	ft_unset(char **str, t_data *data)
 		before = NULL;
 		while (tmp)
 		{
-			// fprintf(stderr, "str[%d] = %s\n", i, str[i]);
-			// fprintf(stderr, "key = %s\t value = %s\n", tmp->key, tmp->value);
 			if (ft_strncmp(tmp->key, str[i], ft_strlen(str[i])) == 0)
 			{
-				// fprintf(stderr, "tmp->key = %s\t tnp->value = %s\n\n", tmp->key, tmp->value);
 				after = tmp->next;
 				free(tmp->key);
 				free(tmp->value);
-				free(tmp);
-				// fprintf(stderr, "tmp->key = %s\t tnp->value = %s\n\n", tmp->key, tmp->value);
 				if (before == NULL)
-				{
 					data->env = *after;
-				}
 				else if (after == NULL)
 				{
+					free(tmp);
 					before->next = NULL;
 				}
 				else
 				{
-					fprintf(stderr, "after->key = %s\tbefore->key = %s\t tmp->key = %s\n\n", after->key, before->key, tmp->key);
+					free(tmp);
 					before->next = after;
 				}
 				break;
@@ -48,15 +42,5 @@ int	ft_unset(char **str, t_data *data)
 		}
 		i++;
 	}
-	tmp = &data->env;
-    while (tmp)
-    {
-
-    	write(1, tmp->key, ft_strlen(tmp->key));
-		write(1, "=", 1);
-		write(1, tmp->value, ft_strlen(tmp->value));
-		write(1, "\n", 1);
-    	tmp = tmp->next;
-    }
 	return (0);
 }
