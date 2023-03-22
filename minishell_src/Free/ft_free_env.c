cfacdd5944280c5528a8ef6ee77b656a6131d2d4
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-03-23 00:18:54 +0100

    norminette ft_free_env

--- minishell_src/Free/ft_free_env.c
+++ minishell_src/Free/ft_free_env.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_free_env.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 00:15:27 by ngriveau          #+#    #+#             */
+/*   Updated: 2023/03/23 00:18:40 by ngriveau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../_Include/minishell.h"
 
-void ft_free_env(t_data *data)
+void	ft_free_env(t_data *data)
 {
 	t_env	*tmp;
 	t_env	*adresse;
