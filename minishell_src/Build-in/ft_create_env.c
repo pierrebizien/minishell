02382a5d070cf36fa51f3a1d16689697591bd26d
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-03-22 20:14:22 +0100

    create_env norminette ok

--- minishell_src/Build-in/ft_create_env.c
+++ minishell_src/Build-in/ft_create_env.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_env.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 19:48:06 by ngriveau          #+#    #+#             */
+/*   Updated: 2023/03/22 20:13:39 by ngriveau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../_Include/minishell.h"
 
 t_env	*ft_lstnew_env(void)
@@ -10,45 +22,52 @@ t_env	*ft_lstnew_env(void)
 	new->key = NULL;
 	new->value = NULL;
 	new->next = NULL;
+	new->printable = 1;
 	return (new);
 }
 
-void ft_modif_SHLVL(t_env *tmp)
+void	ft_modif_shlvl(t_env *tmp)
 {
-	int nb;
+	int	nb;
 
 	nb = ft_atoi(tmp->value);
 	free(tmp->value);
 	tmp->value = ft_itoa(nb + 1);
-
 }
 
-int ft_create_env(char **envp, t_data *data)
+int	ft_create_env_pt2(char **envp, t_env *tmp, int i)
 {
-	int i;
-	int j;
-	int k;
+	int		j;
+	int		k;
+
+	j = 0;
+	while (envp[i][j] != '=' && envp[i][j])
+		j++;
+	k = 0;
+	while (envp[i][k + j + 1])
+		k++;
+	tmp->key = ft_substr(envp[i], 0, j);
+	if (!tmp->key)
+		return (1);
+	tmp->value = ft_substr(envp[i], j + 1, k);
+	if (!tmp->value)
+		return (1);
+	return (0);
+}
+
+int	ft_create_env(char **envp, t_data *data)
+{
+	int		i;
 	t_env	*tmp;
 
 	i = -1;
 	tmp = &data->env;
 	while (envp[++i])
 	{
-		j = 0;
-		while (envp[i][j] != '=' && envp[i][j])
-			j++;
-		k = 0;
-		while (envp[i][k + j + 1])
-			k++;
-		tmp->key = ft_substr(envp[i], 0, j);
-		if (!tmp->key)
+		if (ft_create_env_pt2(envp, tmp, i) == 1)
 			return (1);
-		tmp->value = ft_substr(envp[i], j + 1, k);
-		if (!tmp->key)
-			return (1);
-		tmp->printable = 1;
-		if (ft_strncmp(tmp->key, "SHLVL",6 ) == 0)
-			ft_modif_SHLVL(tmp);
+		if (ft_strncmp(tmp->key, "SHLVL", 6) == 0)
+			ft_modif_shlvl(tmp);
 		if (envp[i + 1])
 		{
 			tmp->next = ft_lstnew_env();
