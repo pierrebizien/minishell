a0028a500e3e6f82d28e781a574cc04513a63fd2
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-03-22 11:11:30 +0100

    fix buildin, (len)

--- minishell_src/Build-in/ft_cmd_test_buildin.c
+++ minishell_src/Build-in/ft_cmd_test_buildin.c
@@ -47,19 +47,19 @@ int ft_exec_builtin(char **cmd, t_data *data, char **cmd_quotes)
 
 int ft_test_builtin(char **cmd)
 {
-	if (ft_strlen(cmd[0]) != 0 && cmd && ft_strncmp(cmd[0], "env", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 3)
+	if (cmd && ft_strlen(cmd[0]) != 0 && ft_strncmp(cmd[0], "env", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 3)
 		return (1);
-	else if (ft_strlen(cmd[0]) != 0 && cmd && ft_strncmp(cmd[0], "export", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 6)
+	else if (cmd && ft_strlen(cmd[0]) != 0 && ft_strncmp(cmd[0], "export", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 6)
 		return (1);
-	else if (ft_strlen(cmd[0]) != 0 && cmd && ft_strncmp(cmd[0], "echo", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 4)
+	else if (cmd && ft_strlen(cmd[0]) != 0 && ft_strncmp(cmd[0], "echo", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 4)
 		return (1);
-	else if (ft_strlen(cmd[0]) != 0 && cmd && ft_strncmp(cmd[0], "cd", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 2)
+	else if (cmd && ft_strlen(cmd[0]) != 0 && ft_strncmp(cmd[0], "cd", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 2)
 		return (1);
-	else if (ft_strlen(cmd[0]) != 0 && cmd && ft_strncmp(cmd[0], "pwd", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 3)
+	else if (cmd && ft_strlen(cmd[0]) != 0 && ft_strncmp(cmd[0], "pwd", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 3)
 		return (1);
-	else if (ft_strlen(cmd[0]) != 0 && cmd && ft_strncmp(cmd[0], "unset", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 5)
+	else if (cmd && ft_strlen(cmd[0]) != 0 && ft_strncmp(cmd[0], "unset", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 5)
 		return (1);
-	else if (ft_strlen(cmd[0]) != 0 && cmd && ft_strncmp(cmd[0], "exit", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 4)
+	else if (cmd && ft_strlen(cmd[0]) != 0 && ft_strncmp(cmd[0], "exit", ft_strlen(cmd[0])) == 0 && ft_strlen(cmd[0]) == 4)
 		return (1);
     else
 		return (0);
