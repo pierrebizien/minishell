700403f35a4d8cc1d944e4debe4341ba8c74fed0
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-03-23 01:04:13 +0100

    pipex_utils.c

--- minishell_src/Utiles/pipex_utils.c
+++ minishell_src/Utiles/pipex_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 01:03:48 by ngriveau          #+#    #+#             */
+/*   Updated: 2023/03/23 01:03:57 by ngriveau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../_Include/minishell.h"
 
 void	ft_free_dchar(char **str)
@@ -14,55 +26,55 @@ void	ft_free_dchar(char **str)
 		free(str);
 }
 
-static void    ft_strrjoin_bis(char *new, char const *s3)
+static void	ft_strrjoin_bis(char *new, char const *s3)
 {
-    size_t    i;
+	size_t	i;
 
-    i = 0;
-    while (s3 && s3[i])
-    {
-        new[i] = s3[i];
-        i++;
-    }
-    new[i] = '\0';
+	i = 0;
+	while (s3 && s3[i])
+	{
+		new[i] = s3[i];
+		i++;
+	}
+	new[i] = '\0';
 }
 
-char    *ft_strrjoin(char const *s1, char const *s2, char const *s3)
+char	*ft_strrjoin(char const *s1, char const *s2, char const *s3)
 {
-    char    *new;
-    size_t    i;
-    size_t    j;
+	char	*new;
+	size_t	i;
+	size_t	j;
 
-    if (!s1 && !s2 && !s3)
-        return (NULL);
-    new = malloc(sizeof(char) * \
-        (ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3) + 1));
-    if (!new)
-        return (NULL);
-    i = 0;
-    while (s1 && s1[i])
-    {
-        new[i] = s1[i];
-        i++;
-    }
-    j = 0;
-    while (s2 && s2[j])
-    {
-        new[i + j] = s2[j];
-        j++;
-    }
-    ft_strrjoin_bis(new + i + j, s3);
-    return (new);
+	if (!s1 && !s2 && !s3)
+		return (NULL);
+	new = malloc(sizeof(char) * \
+		(ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3) + 1));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s1 && s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2 && s2[j])
+	{
+		new[i + j] = s2[j];
+		j++;
+	}
+	ft_strrjoin_bis(new + i + j, s3);
+	return (new);
 }
 
-void    ft_close_all(t_pip pip, t_data *data)
+void	ft_close_all(t_pip pip, t_data *data)
 {
-    ft_close(&pip.fd_in);
-    ft_close(&pip.fd_out);
-    ft_close(&data->pip.saved_stdin);
-    ft_close(&data->pip.saved_stdout);
-    ft_close(&pip.pipefd1[0]);
-    ft_close(&pip.pipefd1[1]);
-    ft_close(&pip.pipefd2[0]);
-    ft_close(&pip.pipefd2[1]);
+	ft_close(&pip.fd_in);
+	ft_close(&pip.fd_out);
+	ft_close(&data->pip.saved_stdin);
+	ft_close(&data->pip.saved_stdout);
+	ft_close(&pip.pipefd1[0]);
+	ft_close(&pip.pipefd1[1]);
+	ft_close(&pip.pipefd2[0]);
+	ft_close(&pip.pipefd2[1]);
 }
